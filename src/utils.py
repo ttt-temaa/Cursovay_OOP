d74fef69961ed201e511c1bed1cda657fc4bb3d0
ttt.temaa <artem1005200776@gmail.com>
2024-10-27 13:34:44 +0300

    creating and starting project files for src

---
+++ src/utils.py
@@ -0,0 +1,28 @@
+from src.vacancy import Vacancy
+
+
+def get_top_vacancies(vacancies: list[Vacancy], n: int) -> list:
+    """Функция, которая возвращает определённый топ вакансий"""
+
+    vacancies.sort(reverse=True)
+    new_list = []
+    for i in range(n):
+        new_list.append(vacancies[i])
+    return new_list
+
+
+def filter_vacancies(vacancies: list[Vacancy], filter_word: str) -> list:
+    """Функция, которая фильтрует вакансии по слову в требованиях"""
+
+    new_list = []
+    for i in vacancies:
+        if filter_word in i.requirement:
+            new_list.append(i)
+
+    return new_list
+
+
+def print_vacancies(vacancies: list[Vacancy]) -> None:
+    """Функция, которая выводит вакансии"""
+    for i in vacancies:
+        print(i)
